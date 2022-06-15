3dc2d672bfe27d0861b35de96176ac50728b0ed5
sunmi <ksm8430058@naver.com>
2022-06-15 17:13:05 +0900

    2022.06.15 재 업로드

---
+++ WpfNaverMovieFinder/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WpfNaverMovieFinder
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearchName_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddWatchList_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
